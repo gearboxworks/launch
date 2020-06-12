b42422cf5add9224ad1a97568816727fb279e2cd
MickMake <github@mickmake.com>
2020-06-12 18:34:34 +1000

    Commit for Release vv1.6.9

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName = "launch"
-	BinaryVersion = "1.6.8"
+	BinaryVersion = "1.6.9"
 	SourceRepo = "github.com/newclarity/launch"
 	BinaryRepo = "github.com/gearboxworks/launch"
 )
--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in v1.3 v1.4 v1.4.1 v1.4.3 v1.6.0 v1.6.1 v1.6.4 v1.6.5 v1.6.6 v1.6.7
+for i in v1.3 v1.4 v1.4.1 v1.4.3 v1.6.0 v1.6.1 v1.6.4 v1.6.5 v1.6.7
 do
 	rm dist/*
 	buildtool sync $i
