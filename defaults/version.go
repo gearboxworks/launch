bc5d16a42274bba1128e68b360618bd5c56d22ba
MickMake <github@mickmake.com>
2021-03-30 13:45:57 +1100

    Update version.go

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName	= "launch"
-	BinaryVersion	= "1.7.4"
+	BinaryVersion	= "1.7.5"
 	SourceRepo	= "github.com/newclarity/" + BinaryName
 	BinaryRepo	= "github.com/gearboxworks/" + BinaryName
 )
