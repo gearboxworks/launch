16b1523091611961020cb1ffd181e43f5b5a5ffd
MickMake <github@mickmake.com>
2021-04-01 18:55:30 +1100

    Commit for Release 1.7.6

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName	= "launch"
-	BinaryVersion	= "1.7.5"
+	BinaryVersion	= "1.7.6"
 	SourceRepo	= "github.com/newclarity/" + BinaryName
 	BinaryRepo	= "github.com/gearboxworks/" + BinaryName
 )
