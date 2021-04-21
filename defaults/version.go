f78f20e595c8fc0a768da95611e75a2797d02cae
MickMake <github@mickmake.com>
2021-04-21 18:42:18 +1000

    Update version.go

--- defaults/version.go
+++ defaults/version.go
@@ -2,7 +2,7 @@ package defaults
 
 const (
 	BinaryName	= "launch"
-	BinaryVersion	= "1.7.9"
+	BinaryVersion	= "1.8.0"
 	SourceRepo	= "github.com/gearboxworks/" + BinaryName
 	BinaryRepo	= "github.com/gearboxworks/" + BinaryName
 )
